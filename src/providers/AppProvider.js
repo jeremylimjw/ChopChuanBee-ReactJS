89c14fef2a781f4bc0aad0fd008d9758d0d83d20
Jeremy <jeremylimjw@gmail.com>
2022-02-06 13:39:09 +0800

    Improved http error handler

--- src/providers/AppProvider.js
+++ src/providers/AppProvider.js
@@ -55,19 +55,21 @@ export function AppProvider({ children }) {
             if (error.response.status === 333) {
                 removeSession();
                 message.error('Login session timed out. Please login again.');
+                throw 'Login session timed out. Please login again.';
             } else {
                 message.error(error.response.data);
+                throw error.response.data;
             }
         } else if (error.request) {
             // The request was made but no response was received
             // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
             // http.ClientRequest in node.js
-            console.log(error.request);
             message.error('The request was made but no response was received.')
+            throw error.request
         } else {
             // Something happened in setting up the request that triggered an Error
-            console.log(error.message);
             message.error('Something happened in setting up the request that triggered an Error.')
+            throw error.message;
         }
     }
 
