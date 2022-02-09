8b9a4138ca7769971205a7994a6647bde65c07cd
Jeremy <jeremylimjw@gmail.com>
2022-02-10 04:04:15 +0800

    Added form template

--- src/pages/MyTemplate.js
+++ src/pages/MyTemplate.js
@@ -1,5 +1,5 @@
 import { PlusOutlined } from '@ant-design/icons/lib/icons'
-import { Button, Form, Input, Table } from 'antd'
+import { Button, Form, Input, InputNumber, Table } from 'antd'
 import React from 'react'
 import MyToolbar from '../components/layout/MyToolbar'
 import MyCard from '../components/layout/MyCard'
@@ -29,12 +29,26 @@ export default function MyTemplate() {
         </MyCard>
 
         <div style={{ display: 'flex'}}>
+          
             <MyCard style={{ flexGrow: 1, margin: '0 12px 24px 24px' }} title="Past Payment History">
-                Bill is a cat.
+
+              <Form labelCol={{ span: 4 }} wrapperCol={{ span: 10 }}>
+                <Form.Item label="Name"><Input /></Form.Item>
+                <Form.Item label="Email"><Input /></Form.Item>
+                <Form.Item label="Age"><InputNumber /> </Form.Item>
+                <Form.Item label="Website"><Input /></Form.Item>
+                <Form.Item label="Introduction"><Input.TextArea /></Form.Item>
+                <Form.Item wrapperCol={{  offset: 4, span: 10 }}>
+                  <Button type="primary" htmlType="submit">Submit</Button>
+                </Form.Item>
+              </Form>
+
             </MyCard>
+
             <MyCard style={{ flexGrow: 1, margin: '0 24px 24px 12px' }} title="Past Deliveries">
                 Bill is a cat.
             </MyCard>
+
         </div>
       </>
   )
