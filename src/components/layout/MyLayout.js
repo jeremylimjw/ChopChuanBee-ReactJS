85985130151de0dc9912b37212651e6fb8a8f8a2
Jeremy <jeremylimjw@gmail.com>
2022-02-10 17:23:34 +0800

    Added a way to change banner content from child component

--- src/components/layout/MyLayout.js
+++ src/components/layout/MyLayout.js
@@ -1,13 +1,17 @@
 import Layout, { Content, Footer } from 'antd/lib/layout/layout'
-import React from 'react'
+import React, { useState } from 'react'
 import Banner from './Banner'
 
-export default function MyLayout({ children, bannerPath, bannerTitle }) {
+export default function MyLayout({ children, bannerPath: path, bannerTitle: title }) {
+
+  const [bannerPath, setBannerPath] = useState(path);
+  const [bannerTitle, setBannerTitle] = useState(title);
+
   return (
     <Layout>
         <Banner paths={bannerPath} title={bannerTitle} />
         <Content>
-            { children }
+            {React.cloneElement(children, { setBannerPath, setBannerTitle } )}
             <Footer style={{ textAlign: 'center', color: 'rgba(0,0,0,.45)' }}>© 2021 Produced by IS4103 Group 3 AY21/22 Sem 2</Footer>
         </Content>
     </Layout>
