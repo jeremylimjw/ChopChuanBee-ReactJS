82ebe423214dd80d53b51ef0ea410ed7d73b7a25
Jeremy <jeremylimjw@gmail.com>
2022-03-03 02:21:08 +0800

    done so add product by alias

--- src/components/common/CustomCell/index.js
+++ src/components/common/CustomCell/index.js
@@ -1,5 +1,6 @@
 import InputCell from "./InputCell";
 import InputNumberCell from "./InputNumberCell";
+import SelectAliasCell from "./SelectAliasCell";
 import SelectProductCell from "./SelectProductCell";
 
 export function CustomCell(props) {
@@ -7,6 +8,7 @@ export function CustomCell(props) {
         case 'input': return InputCell(props);
         case 'input_number': return InputNumberCell(props);
         case 'product_select': return SelectProductCell(props);
+        case 'alias_select': return SelectAliasCell(props);
         default: return <td {...props}>{props.children}</td>
     }
 }
