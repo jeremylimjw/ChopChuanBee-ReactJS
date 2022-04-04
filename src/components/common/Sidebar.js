239a8f438921850dbec732c5a82350796229da69
Regini Lee <e0313703@u.nus.edu>
2022-04-04 20:33:20 +0800

    Manually copied over Sidebar and Index from master branch

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -1,102 +1,120 @@
-import React from 'react';
-import { Layout, Menu } from 'antd';
-import { Link } from 'react-router-dom';
-import {
-    HomeOutlined,
-    InboxOutlined,
-    ShoppingOutlined,
-    SolutionOutlined,
-    TeamOutlined,
-    UserOutlined,
-    LineChartOutlined,
-} from '@ant-design/icons/lib/icons';
-import { useApp } from '../../providers/AppProvider';
+import React from 'react'
+import { Layout, Menu } from 'antd'
+import { Link } from 'react-router-dom'
+import { HomeOutlined, InboxOutlined, ShoppingOutlined, SolutionOutlined, TeamOutlined, UserOutlined, CarOutlined, AccountBookOutlined, LineChartOutlined } from '@ant-design/icons/lib/icons'
+import { useApp } from '../../providers/AppProvider'
+import { useLocation } from "react-router-dom";
+import { View } from '../../enums/View'
+
 
 // Add on more menu items here
 const menu = [
-    {
-        role: 'Admin',
-        title: 'Admin',
-        icon: <UserOutlined />,
-        items: [
-            { route: '/admin/accounts', name: 'Manage Accounts' },
-            { route: '/admin/logs', name: 'Logs' },
-        ],
-    },
-    {
-        role: 'HR',
-        title: 'Human Resource',
-        icon: <TeamOutlined />,
-        items: [
-            { route: '/humanResource/employees', name: 'Manage Employees' },
-            { route: '/humanResource/leaveApplications', name: 'Leave Applications' },
-        ],
-    },
-    {
-        role: 'Inventory',
-        title: 'Inventory',
-        icon: <InboxOutlined />,
-        items: [
-            { route: '/inventory/products', name: 'Manage Products' },
-            // { route: '/inventory', name: 'Manage Inventory' },
-            // { route: '/inventory/supplier-invoices', name: 'Supplier Invoices' },
-        ],
-    },
-    {
-        role: 'SCM',
-        title: 'Supplier',
-        icon: <ShoppingOutlined />,
-        items: [
-            { route: '/supplier/suppliers', name: 'Manage Suppliers' },
-            // { route: '/suppliers/accounts', name: 'Accounts Payable' },
-        ],
-    },
-    {
-        role: 'CRM',
-        title: 'Customer',
-        icon: <SolutionOutlined />,
-        items: [{ route: '/customer/customers', name: 'Manage Customers' }],
-    },
-    {
-        role: 'ACCOUNTING',
-        title: 'Analytics',
-        icon: <LineChartOutlined />,
-        items: [{ route: '/analytics/todayDashboard', name: 'Today Dashboard' },
-                { route: '/analytics/profitabilityDashboard', name: 'Profitability Dashboard' },
-                { route: '/analytics/paymentsDashboard', name: 'Payments Dashboard' },        
-                { route: '/analytics/inventoryDashboard', name: 'Inventory Dashboard'},
-                { route: '/analytics/productDashboard', name: 'Product Dashboard'},
-        ],
-    },
-];
+  {
+    role: View.ADMIN.name,
+    title: 'Admin',
+    icon: <UserOutlined />,
+    items: [
+      { route: '/admin/accounts', name: 'Manage Accounts' },
+      { route: '/admin/companyDetails', name: 'Company Details' },
+      { route: '/admin/logs', name: 'Logs' },
+    ]
+  },
+  {
+    role: View.HR.name,
+    title: 'Human Resource',
+    icon: <TeamOutlined />,
+    items: [
+      { route: '/humanResource/employees', name: 'Manage Employees' },
+      { route: '/humanResource/leaveApplications', name: 'Leave Applications' },
+    ]
+  },
+  {
+    role: View.INVENTORY.name,
+    title: 'Inventory',
+    icon: <InboxOutlined />,
+    items: [
+      { route: '/inventory/products', name: 'Manage Products' },
+      { route: '/inventory/movements', name: 'Inv. Movements' },
+    ]
+  },
+  {
+    role: View.SCM.name,
+    title: 'Supplier',
+    icon: <ShoppingOutlined />,
+    items: [
+      { route: '/supplier/suppliers', name: 'Manage Suppliers' },
+      { route: '/supplier/procurements', name: 'Procurements' },
+    ]
+  },
+  {
+    role: View.CRM.name,
+    title: 'Customer',
+    icon: <SolutionOutlined />,
+    items: [
+      { route: '/customer/customers', name: 'Manage Customers' },
+      { route: '/customer/sales', name: 'Sales' },
+    ]
+  },
+  {
+    role: View.ACCOUNTING.name,
+    title: 'Accounting',
+    icon: <AccountBookOutlined />,
+    items: [
+      { route: '/accounting/balanceSheets', name: 'Balance Sheets' },
+      { route: '/accounting/incomeStatements', name: 'Income Statements' },
+      { route: '/accounting/taxStatements', name: 'Tax Statements' },
+    ]
+  },
+  {
+    role: 'ACCOUNTING',
+    title: 'Analytics',
+    icon: <LineChartOutlined />,
+    items: [
+      { route: '/analytics/todayDashboard', name: 'Today Dashboard' },
+      { route: '/analytics/profitabilityDashboard', name: 'Profitability Dashboard' },
+      { route: '/analytics/paymentsDashboard', name: 'Payments Dashboard' },        
+      { route: '/analytics/inventoryDashboard', name: 'Inventory Dashboard'},
+      { route: '/analytics/productDashboard', name: 'Product Dashboard'},
+    ],
+  },
+  {
+    role: View.DISPATCH.name,
+    title: 'Dispatch',
+    icon: <CarOutlined />,
+    items: [
+      { route: '/dispatch/itinerarys', name: 'Manage Itineraries' },
+      { route: '/dispatch/deliveryOrders', name: 'Manage Deliveries' },
+    ]
+  },
+]
 
-const Sidebar = () => {
+export default function Sidebar() {
+
     const { hasViewAccessTo } = useApp();
+    const location = useLocation();
 
     return (
-        <Layout.Sider theme='light' width={210} style={{ boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px' }}>
-            <Menu mode='inline'>
-                <Menu.Item key='home' icon={<HomeOutlined />}>
-                    <Link to='/'>Home</Link>
-                </Menu.Item>
+      <Layout.Sider theme='light' width={210} style={{ boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px' }}>
+        <Menu mode='inline' selectedKeys={[location.pathname]}>
+          <Menu.Item key='/' icon={<HomeOutlined />}>
+            <Link to='/'>Home</Link>
+          </Menu.Item>
 
-                {menu.map((menuItem, index) => {
-                    if (hasViewAccessTo(menuItem.role)) {
-                        return (
-                            <Menu.SubMenu key={index} title={menuItem.title} icon={menuItem.icon}>
-                                {menuItem.items.map((subMenu, index2) => (
-                                    <Menu.Item key={`${index}_${index2}`}>
-                                        <Link to={subMenu.route}>{subMenu.name}</Link>
-                                    </Menu.Item>
-                                ))}
-                            </Menu.SubMenu>
-                        );
-                    }
-                    return null;
-                })}
-            </Menu>
-        </Layout.Sider>
+          {menu.map((menuItem, index) => {
+            if (hasViewAccessTo(menuItem.role)) {
+              return (
+                <Menu.SubMenu key={index} title={menuItem.title} icon={menuItem.icon}>
+                  {menuItem.items.map((subMenu, index2) => (
+                    <Menu.Item key={subMenu.route}>
+                      <Link to={subMenu.route}>{subMenu.name}</Link>
+                    </Menu.Item>
+                  ))}
+                </Menu.SubMenu>
+              );
+            }
+            return null;
+          })}
+        </Menu>
+      </Layout.Sider>
     );
 };
-
-export default Sidebar;
--- src/index.js
+++ src/index.js
@@ -25,6 +25,27 @@ import ManageSuppliersPage from './components/supplier/ManageSuppliersPage';
 import ViewSupplierPage from './components/supplier/ViewSupplierPage';
 import ManageCustomersPage from './components/customer/ManageCustomersPage';
 import ViewCustomerPage from './components/customer/ViewCustomerPage';
+import ManageBalanceSheetPage from './components/accounting/ManageBalanceSheetPage';
+import ViewBalanceSheetPage from './components/accounting/ViewBalanceSheetPage';
+import ViewIncomeStatementPage from './components/accounting/ViewIncomeStatementPage';
+import ManageIncomeStatementPage from './components/accounting/ManageIncomeStatementPage';
+import ManageTaxStatementPage from './components/accounting/ManageTaxStatementPage';
+import ManageProcurementsPage from './components/supplier/ManageProcurementsPage';
+import NewProcurementPage from './components/supplier/NewProcurementPage';
+import ViewProcurementPage from './components/supplier/ViewProcurementPage';
+import ManageChargedUndersPage from './components/admin/ManageChargedUndersPage';
+import ViewChargedUnderPage from './components/admin/ViewChargedUnderPage';
+import ManageSalesOrdersPage from './components/customer/ManageSalesOrdersPage';
+import NewSalesOrderPage from './components/customer/NewSalesOrderPage';
+import ViewSalesOrderPage from './components/customer/ViewSalesOrderPage';
+import ViewInventoryMovementsPage from './components/inventory/ViewInventoryMovementsPage';
+import ManageDeliveriesPage from './components/dispatch/ManageDeliveriesPage';
+import ActivatePage from './components/ActivatePage';
+import ViewSORAPage from './components/customer/ViewSORAPage';
+import ManageItinerarysPage from './components/dispatch/ManageItinerarysPage';
+import NewItineraryPage from './components/dispatch/NewItineraryPage';
+import ViewItineraryPage from './components/dispatch/ViewItineraryPage';
+import CompleteDeliveryPage from './components/CompleteDeliveryPage';
 import TodayDashboard from './components/analytics/TodayDashboard';
 import PaymentsDashboard from './components/analytics/PaymentsDashboard';
 import ProfitabilityDashboard from './components/analytics/ProfitabilityDashboard';
@@ -33,139 +54,246 @@ import ProductDashboard from './components/analytics/ProductDashboard';
 
 // Add on more routes here
 const routes = [
-    {
-        path: '/',
-        component: <MyTemplate />,
-    },
-    {
-        path: '/admin',
-        component: <Outlet />,
-        childRoutes: [
-            {
-                path: 'accounts',
-                component: <ManageAccountsPage />,
-                viewAccess: View.ADMIN.name,
-            },
-            {
-                path: 'accounts/:id',
-                component: <ViewAccountPage />,
-                viewAccess: View.ADMIN.name,
-            },
-            {
-                path: 'logs',
-                component: <ViewLogsPage />,
-                viewAccess: View.ADMIN.name,
-            },
-        ],
-    },
-    {
-        path: '/myProfile',
-        component: <MyProfilePage />,
-    },
-    {
-        path: '/myLeaves',
-        component: <MyLeavePage />,
-    },
-    {
-        path: 'humanResource',
-        component: <Outlet />,
-        childRoutes: [
-            {
-                path: 'employees',
-                component: <ManageEmployeesPage />,
-                viewAccess: View.HR.name,
-            },
-            {
-                path: 'employees/:id',
-                component: <ViewEmployeePage />,
-                viewAccess: View.HR.name,
-            },
-            {
-                path: 'leaveApplications',
-                component: <ManageLeavesPage />,
-                viewAccess: View.HR.name,
-            },
-        ],
-    },
-    {
-        path: '/inventory',
-        component: <Outlet />,
-        childRoutes: [
-            {
-                path: 'products',
-                component: <ManageProductsPage />,
-                viewAccess: View.INVENTORY.name,
-            },
-            {
-                path: 'products/:id',
-                component: <ViewProductPage />,
-                viewAccess: View.INVENTORY.name,
-            },
-        ],
-    },
-    {
-        path: '/supplier',
-        component: <Outlet />,
-        childRoutes: [
-            {
-                path: 'suppliers',
-                component: <ManageSuppliersPage />,
-                viewAccess: View.SCM.name,
-            },
-            {
-                path: 'suppliers/:id',
-                component: <ViewSupplierPage />,
-                viewAccess: View.SCM.name,
-            },
-        ],
-    },
-    {
-        path: '/customer',
-        component: <Outlet />,
-        childRoutes: [
-            {
-                path: 'customers',
-                component: <ManageCustomersPage />,
-                viewAccess: View.CRM.name,
-            },
-            {
-                path: 'customers/:id',
-                component: <ViewCustomerPage />,
-                viewAccess: View.CRM.name,
-            },
-        ],
-    },
-    {
-        path: '/analytics',
-        component: <Outlet />,
-        childRoutes: [
-            {
-                path: 'todayDashboard',
-                component: <TodayDashboard />,
-                viewAccess: View.ANALYTICS.name,
-            },
-            {
-                path: 'paymentsDashboard',
-                component: <PaymentsDashboard />,
-                viewAccess: View.ANALYTICS.name,
-            },
-            {
-                path: 'profitabilityDashboard',
-                component: <ProfitabilityDashboard />,
-                viewAccess: View.ANALYTICS.name,
-            },
-            {
-                path: 'inventoryDashboard',
-                component: <InventoryDashboard />,
-                viewAccess: View.ANALYTICS.name,
-            },
-            {
-                path: 'productDashboard',
-                component: <ProductDashboard />,
-                viewAccess: View.ANALYTICS.name,
-            },
-        ],
-    },
+  {
+    path: '/',
+    component: <MyTemplate />,
+  },
+  {
+    path: '/admin',
+    component: <Outlet />,
+    childRoutes: [
+      { 
+        path: 'accounts', 
+        component: <ManageAccountsPage />,
+        viewAccess: View.ADMIN.name,
+      },
+      { 
+        path: 'accounts/:id', 
+        component: <ViewAccountPage />,
+        viewAccess: View.ADMIN.name,
+      },
+      { 
+        path: 'companyDetails', 
+        component: <ManageChargedUndersPage />,
+        viewAccess: View.ADMIN.name,
+      },
+      { 
+        path: 'companyDetails/:id', 
+        component: <ViewChargedUnderPage />,
+        viewAccess: View.ADMIN.name,
+      },
+      { 
+        path: 'logs', 
+        component: <ViewLogsPage />,
+        viewAccess: View.ADMIN.name,
+      },
+    ]
+  },
+  {
+    path: '/myProfile',
+    component: <MyProfilePage />,
+  },
+  {
+    path: '/myLeaves',
+    component: <MyLeavePage />,
+  },
+  {
+    path: 'humanResource',
+    component: <Outlet />,
+    childRoutes: [
+      { 
+        path: 'employees', 
+        component: <ManageEmployeesPage />,
+        viewAccess: View.HR.name
+      },
+      { 
+        path: 'employees/:id', 
+        component: <ViewEmployeePage />,
+        viewAccess: View.HR.name
+      },
+      { 
+        path: 'leaveApplications', 
+        component: <ManageLeavesPage />,
+        viewAccess: View.HR.name
+      },
+    ]
+  },
+  {
+    path: '/inventory',
+    component: <Outlet />,
+    childRoutes: [
+      { 
+        path: 'products', 
+        component: <ManageProductsPage />,
+        viewAccess: View.INVENTORY.name,
+      },
+      { 
+        path: 'products/:id', 
+        component: <ViewProductPage />,
+        viewAccess: View.INVENTORY.name,
+      },
+      { 
+        path: 'movements', 
+        component: <ViewInventoryMovementsPage />,
+        viewAccess: View.INVENTORY.name,
+      },
+    ]
+  },
+  {
+    path: "/supplier",
+    component: <Outlet />,
+    childRoutes: [
+      {
+        path: "suppliers",
+        component: <ManageSuppliersPage />,
+        viewAccess: View.SCM.name,
+      },
+      {
+        path: "suppliers/:id",
+        component: <ViewSupplierPage />,
+        viewAccess: View.SCM.name,
+      },
+      { 
+        path: 'procurements', 
+        component: <ManageProcurementsPage />,
+        viewAccess: View.SCM.name,
+      },
+      { 
+        path: 'procurements/new', 
+        component: <NewProcurementPage />,
+        viewAccess: View.SCM.name,
+      },
+      { 
+        path: 'procurements/:id', 
+        component: <ViewProcurementPage />,
+        viewAccess: View.SCM.name,
+      },
+    ],
+  },
+  {
+    path: '/customer',
+    component: <Outlet />,
+    childRoutes: [
+      { 
+        path: 'customers', 
+        component: <ManageCustomersPage />,
+        viewAccess: View.CRM.name,
+      },
+      { 
+        path: 'customers/:id', 
+        component: <ViewCustomerPage />,
+        viewAccess: View.CRM.name,
+      },
+      { 
+        path: 'sales', 
+        component: <ManageSalesOrdersPage />,
+        viewAccess: View.SCM.name,
+      },
+      { 
+        path: 'sales/new', 
+        component: <NewSalesOrderPage />,
+        viewAccess: View.SCM.name,
+      },
+      { 
+        path: 'sales/:id', 
+        component: <ViewSalesOrderPage />,
+        viewAccess: View.SCM.name,
+      },
+      { 
+        path: 'customers/SORA/:id', 
+        component: <ViewSORAPage />,
+        viewAccess: View.SCM.name,
+      },
+    ]
+  },
+  {
+    path: "/accounting",
+    component: <Outlet />,
+    childRoutes: [
+      {
+        path: "balanceSheets",
+        component: <ManageBalanceSheetPage />,
+        viewAccess: View.ACCOUNTING.name,
+      },
+      {
+        path: "balanceSheets/:id",
+        component: <ViewBalanceSheetPage />,
+        viewAccess: View.ACCOUNTING.name,
+      },
+      {
+        path: "incomeStatements",
+        component: <ManageIncomeStatementPage />,
+        viewAccess: View.ACCOUNTING.name,
+      },
+      {
+        path: "incomeStatements/:id",
+        component: <ViewIncomeStatementPage />,
+        viewAccess: View.ACCOUNTING.name,
+      },
+      {
+        path: "taxStatements",
+        component: <ManageTaxStatementPage />,
+        viewAccess: View.ACCOUNTING.name,
+      },
+    ],
+  },
+  { 
+    path: '/dispatch',
+    component: <Outlet />,
+    childRoutes: [
+      { 
+        path: 'itinerarys', 
+        component: <ManageItinerarysPage />,
+        viewAccess: View.DISPATCH.name,
+      },
+      { 
+        path: 'itinerarys/new', 
+        component: <NewItineraryPage />,
+        viewAccess: View.DISPATCH.name,
+      },
+      { 
+        path: 'itinerarys/:id', 
+        component: <ViewItineraryPage />,
+        viewAccess: View.DISPATCH.name,
+      },
+      { 
+        path: 'deliveryOrders', 
+        component: <ManageDeliveriesPage />,
+        viewAccess: View.DISPATCH.name,
+      },
+    ]
+  },
+  {
+    path: '/analytics',
+    component: <Outlet />,
+    childRoutes: [
+      {
+        path: 'todayDashboard',
+        component: <TodayDashboard />,
+        viewAccess: View.ANALYTICS.name,
+      },
+      {
+        path: 'paymentsDashboard',
+        component: <PaymentsDashboard />,
+        viewAccess: View.ANALYTICS.name,
+      },
+      {
+        path: 'profitabilityDashboard',
+        component: <ProfitabilityDashboard />,
+        viewAccess: View.ANALYTICS.name,
+      },
+      {
+        path: 'inventoryDashboard',
+        component: <InventoryDashboard />,
+        viewAccess: View.ANALYTICS.name,
+      },
+      {
+        path: 'productDashboard',
+        component: <ProductDashboard />,
+        viewAccess: View.ANALYTICS.name,
+      },
+    ],
+  },
 ];
 
 function renderRoute(route, index) {
@@ -203,28 +331,23 @@ function renderRoute(route, index) {
 }
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Router>
-            <AppProvider>
-                <Layout style={{ minHeight: '100vh' }}>
-                    <Routes>
-                        <Route path='/login' element={<LoginPage />} />
-                        <Route
-                            path='/'
-                            element={
-                                <RequireAuth>
-                                    <App />
-                                </RequireAuth>
-                            }
-                        >
-                            {routes.map((route, index) => renderRoute(route, index))}
-                        </Route>
-                    </Routes>
-                </Layout>
-            </AppProvider>
-        </Router>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Router>
+      <AppProvider>
+        <Layout style={{ minHeight: '100vh' }}>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/activate" element={<ActivatePage />} />
+            <Route path="/completeDelivery" element={<CompleteDeliveryPage />} />
+            <Route path="/" element={<RequireAuth><App /></RequireAuth>}>
+              {routes.map((route, index) => renderRoute(route, index))}
+            </Route>
+          </Routes>
+        </Layout>
+      </AppProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
