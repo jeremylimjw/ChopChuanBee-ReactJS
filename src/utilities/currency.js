0220e68b7b550da2357533972c130fb8cc67d3ce
Jeremy <jeremylimjw@gmail.com>
2022-04-04 20:46:00 +0800

    cleanup

--- src/utilities/currency.js
+++ src/utilities/currency.js
@@ -2,10 +2,10 @@ export function formatCurrency(amount) {
     var is_negative = amount < 0;
     var decimal = parseFloat(amount).toFixed(2);
     var parts = decimal.toString().split(".");
-
+    
     if (is_negative) {
-        return '-' + "$" + parts[0].substring(1, parts[0].length).replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : "");
+        return `-$${parts[0].substring(1, parts[0].length).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${parts[1] ? "." + parts[1] : ""}`;
     } else {
-        return "$" + parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : "");
+        return `$${parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${parts[1] ? "." + parts[1] : ""}`;
     }
 }
